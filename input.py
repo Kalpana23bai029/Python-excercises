2bdf82af0c56f1563f1056cb8202c5d6ff91f9c0
Kalpana Sai <kalpanasai638@gmail.com>
2025-05-25 14:09:50 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,13 @@
+#input()
+
+input("What is Your Name:")
+
+
+# input using f string
+
+name=input("What is Your Name?:")
+age=int(input("Enter Your age:"))
+age=age + 1
+print(f"Hello{name}!!")
+print("HAPPY BIRTHDAY")
+print(f"Your Age Is {age}")
